f9f4f9761f81e08fe737b61ad4aef321ab2ee9c5
DESKTOP-6L61UUP\JIANG三工 <2662774600@qq.com>
2023-02-26 17:15:30 +0800

    提交发布版本的内容

---
+++ cc_2_4_8/web-mobile/src/settings.72d0a.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/temp/scripts/frame/global/Index.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d17e2",main:"0da6b"}};
